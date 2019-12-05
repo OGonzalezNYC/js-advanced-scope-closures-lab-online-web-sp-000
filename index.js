8981571325fb069a197ef82d349e3d8c8e59703f
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-12-05 01:09:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function produceDrivingRange(blockRange) {
+  return function(start, stop) {
+    let startNumber = parseInt(start.slice(0, 2));
+    let stopNumber = parseInt(stop.slice(0, 2));
+    let distance = (stopNumber - startNumber);
+    if (distance <= blockRange) {
+      return `within range by ${ blockRange - distance}`
+    } else {
+      return `${distance - blockRange} blocks out of range`
+    }
+  }
+}
