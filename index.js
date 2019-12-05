f38447c3b7e9dac6f81b6e9ff3f27c67fe4f5247
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-12-05 01:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function produceTipCalculator(tipPercentage) {
 function createDriver() {
   let driverId = 0;
   return class Driver {
-    constructor() {
+    constructor(name) {
+      this.name = name;
       this.id = ++driverId;
     }
   }
